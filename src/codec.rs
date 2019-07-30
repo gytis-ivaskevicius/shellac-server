80074e4fcaa03ec3473a73d82d2ab34b28450f45
Xavier L'Heureux <xavier.lheureux@icloud.com>
2019-07-30 09:28:59 -0400

    Send exact strings length to avoid allocations + support stdin/out

--- src/codec.rs
+++ src/codec.rs
@@ -1,26 +1,32 @@
-use std::io::{self, BufRead, Read};
+use std::io::{self, ErrorKind, Read};
+use std::mem;
 
 use super::AutocompRequest;
 use super::Error;
 
 #[derive(Debug)]
-pub struct ArgvCodec<R: Read + BufRead> {
+pub struct ArgvCodec<R: Read> {
     reader: R,
 }
 
-impl<R: Read + BufRead> ArgvCodec<R> {
+impl<R: Read> ArgvCodec<R> {
     pub fn new(reader: R) -> Self {
         Self { reader }
     }
 }
 
-impl<R: Read + BufRead> ArgvCodec<R> {
+impl<R: Read> ArgvCodec<R> {
     pub fn decode(&mut self) -> Result<Option<AutocompRequest>, Error> {
-        if self.reader.fill_buf()?.len() == 0 {
-            return Ok(None);
-        }
         let mut argc = [0u8; 2];
-        self.reader.read_exact(&mut argc)?;
+        match self.reader.read(&mut argc)? {
+            0 => return Ok(None),
+            1 => {
+                return Err(
+                    io::Error::new(ErrorKind::UnexpectedEof, "failed to fill whole buffer").into(),
+                )
+            }
+            _ => (),
+        }
         let argc = u16::from_be_bytes(argc) as usize;
 
         let mut word = [0u8; 2];
@@ -31,22 +37,12 @@ impl<R: Read + BufRead> ArgvCodec<R> {
         }
 
         std::iter::repeat_with(|| {
-            let mut string = Vec::with_capacity(32);
-            if self.reader.fill_buf()?.len() == 0 {
-                return Err(io::Error::new(
-                    io::ErrorKind::UnexpectedEof,
-                    format!("Protocol failure, wrong argc ({})", argc),
-                )
-                .into());
-            }
-            self.reader.read_until(b'\0', &mut string)?;
-            if string.pop() != Some(b'\0') {
-                return Err(io::Error::new(
-                    io::ErrorKind::UnexpectedEof,
-                    "Protocol failure, an argument was improperly null terminated",
-                )
-                .into());
-            }
+            let mut len = [0u8; mem::size_of::<usize>()];
+            self.reader.read_exact(&mut len)?;
+            let len = usize::from_be_bytes(len);
+
+            let mut string = vec![0u8; len];
+            self.reader.read_exact(&mut string)?;
             // Convert the bytes to a string and error if the bytes are not valid utf-8.
             String::from_utf8(string).map_err(Into::into)
         })
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use shellac_server::{codec::ArgvCodec, completion};
 
 use std::fs::{self, File};
-use std::io::{self, BufReader, BufWriter, Write};
-use std::os::unix::net::{UnixListener, UnixStream};
+use std::io::{self, BufReader, BufWriter, Read, Write};
+use std::os::unix::net::UnixListener;
 use std::path::{Path, PathBuf};
 use std::thread;
 
@@ -41,11 +41,11 @@ fn get_comp_file(argv0: &str) -> io::Result<PathBuf> {
     Ok(path.with_extension("shellac"))
 }
 
-fn handle_client(stream: UnixStream) -> Result<(), shellac_server::Error> {
+fn handle_client<R: Read, W: Write>(reader: R, writer: W) -> Result<(), shellac_server::Error> {
     use std::time::Instant;
 
-    let mut writer = BufWriter::new(&stream);
-    let mut codec = ArgvCodec::new(BufReader::new(&stream));
+    let mut writer = BufWriter::new(writer);
+    let mut codec = ArgvCodec::new(BufReader::new(reader));
     while let Some(request) = codec.decode()? {
         let start = Instant::now();
         let path = get_comp_file(&request.argv()[0])?;
@@ -53,8 +53,7 @@ fn handle_client(stream: UnixStream) -> Result<(), shellac_server::Error> {
         let completed = completion::complete(file, request)?;
         let duration = start.elapsed();
         serde_json::to_writer(&mut writer, &completed);
-        writer.flush()?;
-        println!("Time elapsed: {:?}", duration);
+        eprintln!("Time elapsed: {:?}", duration);
     }
     Ok(())
 }
@@ -62,27 +61,30 @@ fn handle_client(stream: UnixStream) -> Result<(), shellac_server::Error> {
 fn main() {
     let opts = Opts::from_args();
 
-    let path = opts
-        .socket
-        .unwrap_or_else(|| PathBuf::from("/run/user/1000/shellac.sock"));
+    if let Some(path) = opts.socket {
+        let listener = UnixListener::bind(&path).unwrap_or_else(|_| {
+            fs::remove_file(&path).unwrap();
+            UnixListener::bind(&path).unwrap()
+        });
 
-    let listener = UnixListener::bind(&path).unwrap_or_else(|_| {
-        fs::remove_file(&path).unwrap();
-        UnixListener::bind(&path).unwrap()
-    });
-
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                thread::spawn(|| match handle_client(stream) {
-                    Ok(()) => println!("Socket closed"),
-                    Err(err) => println!("Could not execute request: {}", err),
-                });
+        for stream in listener.incoming() {
+            match stream {
+                Ok(stream) => {
+                    thread::spawn(move || match handle_client(&stream, &stream) {
+                        Ok(()) => eprintln!("Socket closed"),
+                        Err(err) => eprintln!("Could not execute request: {}", err),
+                    });
+                }
+                Err(err) => {
+                    eprintln!("Could not accept socket request {}", err);
+                    break;
+                }
             }
-            Err(err) => {
-                println!("Could not accept socket request {}", err);
-                break;
-            }
+        }
+    } else {
+        match handle_client(io::stdin().lock(), io::stdout().lock()) {
+            Ok(()) => eprintln!("Socket closed"),
+            Err(err) => eprintln!("Could not execute request: {}", err),
         }
     }
 }
